a8c66ac50f6d9bc38fb8aa408b0fcc62c1ec0fe5
Doug <doug@dougflynn.dev>
2024-11-19 20:45:51 +0900

    feat: consume theme-selected event in extension
    This commit adds the meat of the extension - appending `link` tags to the document head.

--- extension/extensionMain.js
+++ extension/extensionMain.js
@@ -1,11 +1,8 @@
-// TEMP FILE TO EXPLORE FUNCTIONALITY :)
-
-console.log('Extension Started');
-
-
-const css = `
-    body { background-color: red !important; color: white !important; }
-    `;
+/**
+ * This is the "main" script - it has access to both the chrome API, and the DOM (via chrome API).
+ * This script listens for the `theme-selected` event from the auro-themeswitcher element, and then
+ * injects the selected themes into the current tab.
+ */
 
 const getTabId = async () => {
   const queryOptions = {
@@ -17,14 +14,36 @@ const getTabId = async () => {
   return tab.id;
 };
 
-(async () => {
-  const tabId = await getTabId();
-  console.log('Tab ID:', tabId);
+const themeSwitcher = document.querySelector('auro-themeswitcher');
 
-  chrome.scripting
-    .insertCSS({
-      target: {tabId},
-      css,
-    })
-    .then(() => console.log("CSS injected"));
-})();
+themeSwitcher.addEventListener('theme-selected', async (event) => {
+  /** @type {Array<{label: string; url: string;}>} **/
+  const themeArr = event.target.currentThemes.map((theme) => JSON.parse(theme));
+  const tabId = await getTabId();
+
+  await chrome.scripting.executeScript({
+    target: { tabId },
+    func: (themes) => {
+      // Access the real DOM within the page context
+      const links = document.head.querySelectorAll('link[data-id^="theme-switcher-link-"]');
+
+      // Remove old theme links
+      for (const link of links) {
+        link.remove();
+      }
+
+      // Add new themes
+      for (const themeEntry of Object.entries(themes)) {
+        const [
+          ident,
+          theme
+        ] = themeEntry;
+        document.head.insertAdjacentHTML(
+          'beforeend',
+          `<link data-id="theme-switcher-link-${ident}" rel="stylesheet" href="${theme.url}"/>`
+        );
+      }
+    },
+    args: [themeArr]
+  });
+});
