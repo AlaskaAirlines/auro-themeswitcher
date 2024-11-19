2b03f25ccd779b028e0ef9075683ce049615a7d6
Doug <doug@dougflynn.dev>
2024-11-19 20:45:51 +0900

    feat: consume theme-selected event in extension
    This commit adds the meat of the extension - appending `link` tags to the document head.

---
+++ extension/prepareDOM.js
@@ -0,0 +1,9 @@
+// --------------------------------------------------
+// This script is run on load, and does some prep
+// work for the theme switcher.
+// --------------------------------------------------
+
+// ADD CODE HERE IF NEEDED!
+
+// eslint-disable-next-line no-console
+console.log('prepareDOM.js loaded');
